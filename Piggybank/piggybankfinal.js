be9f21c4574ca7409626a37b0074e1478c15a669
tpali <tpali@hawaii.edu>
2015-12-08 19:56:26 -1000

    Final edits to the html and javascript files

--- Piggybank/piggybankfinal.js
+++ Piggybank/piggybankfinal.js
@@ -87,7 +87,6 @@ function updateBalance(e) {
 	}
 }
 
-
 // Function to update teh budget through the HTML form
 // Also does error checking on the input
 // e is an event since this function is passed from html through an event
